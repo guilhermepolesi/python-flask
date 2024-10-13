6d077d4aadbb3512490dafd418c8037cc741ae55
guilhermepolesi <guilherme-polesi@hotmail.com>
2024-10-13 11:33:13 -0300

    Adding Bootstrap and Template Inheritance

--- tutorials/web-app/src/main.py
+++ tutorials/web-app/src/main.py
@@ -2,13 +2,18 @@ from flask import Flask, redirect, url_for, render_template
 
 app = Flask(__name__)
 
-@app.route("/<name>")
-def home(name):
-    return render_template("index.html", content=["Wolverine", "Rogue", "Bishop"])
+@app.route("/")
+def home():
+    return render_template("index.html")
+
+
+@app.route("/test")
+def test():
+    return render_template("new.html")
 
 
 if __name__ == "__main__":
-    app.run()
+    app.run(debug=True)
 
 
 
