76eb8cc15d4ca9ca74e4f4001fce6f35210b2a2d
guilhermepolesi <guilherme-polesi@hotmail.com>
2024-10-14 23:05:32 -0300

    Adding Users SQLAlchemy

--- tutorials/web-app/src/main.py
+++ tutorials/web-app/src/main.py
@@ -25,12 +25,26 @@ def home():
     return render_template("index.html")
 
 
+@app.route("/view")
+def view():
+    return render_template("view.html", values=Users.query.all())
+
+
 @app.route("/login", methods=["POST", "GET"])
 def login():
     if request.method == "POST":
         session.permanent = True
         user = request.form["nm"]
         session["user"] = user
+
+        found_user = Users.query.filter_by(name=user).first()
+        if found_user:
+            session["email"] = found_user.email
+        else:
+            usr = Users(user, "")
+            db.session.add(usr)
+            db.session.commit()
+
         flash("Login Successful!")
         return redirect(url_for("user"))
     else:
@@ -50,6 +64,9 @@ def user():
         if request.method == "POST":
             email = request.form["email"]
             session["email"] = email
+            found_user = Users.query.filter_by(name=user).first()
+            found_user.email = email
+            db.session.commit()
             flash("Email was saved!")
 
         else:
